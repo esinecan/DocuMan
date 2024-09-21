915c082df34dd6b1dd1b74b653b6cddb95c1c4aa
Eren Can Sinecan <esinecan@gmail.com>
2024-09-21 11:33:39 +0000

    disable swGGER temporarily

---
+++ src/main/java/com/documentsies/DocuMan/config/SwaggerConfig.java
@@ -0,0 +1,12 @@
+package com.documentsies.DocuMan.config;
+
+//@Configuration
+//@EnableSwagger2
+public class SwaggerConfig {
+
+    /*
+     * @Bean public Docket api() { return new Docket(DocumentationType.SWAGGER_2).select()
+     * .apis(RequestHandlerSelectors.basePackage("com.documentsies.DocuMan.controller"))
+     * .paths(PathSelectors.any()).build(); }
+     */
+}
